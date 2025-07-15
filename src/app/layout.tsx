4568dfb728fe991703fedec4dc04a97a735dc152
joytofu <kcswag@foxmail.com>
2025-07-15 22:35:41 +0800

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,6 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en-US" className="dark">
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
       <body className={inter.className}>{children}</body>
     </html>
   );
